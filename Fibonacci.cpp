0632227c0ff4f631a58e88d4b0bd02b839beddfc
Debian <debian@debian10.linuxvmimages.local>
2021-04-30 07:11:52 -0500

    Third

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,3 +1,30 @@
-indnfjdfnd
-dfdfdf
-dfdfd
+#include<stdio.h>
+
+int main(){
+	int x;
+	printf("Enter your age");
+	scanf("%d",&x);
+	int temp = 0;
+	int a = 0, b = 1, s = 0;
+	while(a+temp<=x){
+	temp=a;
+	a=a+b;
+	b=temp;
+	s=s+a;
+	}
+	printf("sum=%d\n",s);
+	
+	int c, k;
+	for(c=31;c>=0; c--)
+	{
+	k= s>>c;
+		if(k&1)
+			printf("1");
+		else
+			printf("0");
+	}
+
+}
+
+
+
